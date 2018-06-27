357afa130ed4e41f663d9c7208e3131775efd423
Richard Barnes <rlb@ipv.sx>
2018-06-27 12:46:26 -0700

    Add SRTP KDF and test

---
+++ srtp-kdf.go
@@ -0,0 +1,63 @@
+package rtp
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+)
+
+const (
+	Ke byte = 0x00
+	Ka byte = 0x01
+	Ks byte = 0x02
+)
+
+type KDF struct {
+	masterSalt []byte
+	block      cipher.Block
+}
+
+func NewKDF(masterKey, masterSalt []byte) (*KDF, error) {
+	if len(masterSalt) != 14 {
+		return nil, fmt.Errorf("SRTP master salt must be 14 bytes long")
+	}
+
+	block, err := aes.NewCipher(masterKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KDF{
+		masterSalt: masterSalt,
+		block:      block,
+	}, nil
+}
+
+func (kdf KDF) Derive(label byte, roc uint64, seq uint16, size int) []byte {
+	indexVal := (roc << 16) + uint64(seq)
+	index := make([]byte, 6)
+	for i := range index {
+		index[5-i] = byte(indexVal)
+		indexVal >>= 8
+	}
+
+	keyID := append([]byte{label}, index...)
+
+	x := make([]byte, len(kdf.masterSalt))
+	copy(x, kdf.masterSalt)
+	start := len(kdf.masterSalt) - len(keyID)
+	for i := range keyID {
+		x[start+i] ^= keyID[i]
+	}
+
+	iv := append(x, []byte{0x00, 0x00}...)
+	stream := cipher.NewCTR(kdf.block, iv)
+
+	out := make([]byte, size)
+	for i := range out {
+		out[i] = 0x00
+	}
+
+	stream.XORKeyStream(out, out)
+	return out
+}
---
+++ srtp-kdf_test.go
@@ -0,0 +1,39 @@
+package rtp
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
+
+// From https://tools.ietf.org/html/rfc3711#appendix-B.3
+func TestKDF(t *testing.T) {
+	masterKeyHex := "E1F97A0D3E018BE0D64FA32C06DE4139"
+	masterSaltHex := "0EC675AD498AFEEBB6960B3AABE6"
+	cipherKeyHex := "C61E7A93744F39EE10734AFE3FF7A087"
+	cipherSaltHex := "30CBBC08863D8C85D49DB34A9AE1"
+
+	cipherKeySize := 16
+	cipherSaltSize := 14
+
+	masterKey, _ := hex.DecodeString(masterKeyHex)
+	masterSalt, _ := hex.DecodeString(masterSaltHex)
+	cipherKey, _ := hex.DecodeString(cipherKeyHex)
+	cipherSalt, _ := hex.DecodeString(cipherSaltHex)
+
+	kdf, err := NewKDF(masterKey, masterSalt)
+	if err != nil {
+		t.Fatalf("Error creating KDF")
+	}
+
+	cipherKeyTest := kdf.Derive(Ke, 0, 0, cipherKeySize)
+	if !bytes.Equal(cipherKeyTest, cipherKey) {
+		t.Fatalf("Incorrect cipher key: %x != %x", cipherKeyTest, cipherKey)
+	}
+
+	cipherSaltTest := kdf.Derive(Ks, 0, 0, cipherSaltSize)
+	if !bytes.Equal(cipherSaltTest, cipherSalt) {
+		t.Fatalf("Incorrect cipher salt: %x != %x", cipherSaltTest, cipherSalt)
+	}
+
+}
